81ba84000e01a28d0d86f3a062d3af8f7da61db3
Moi-cy <mio@moi-cy.de>
2026-09-20 03:08:47 +0200

    Implement mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const menuButton = document.getElementById("menuButton");
+const navLinks = document.getElementById("navLinks");
 
+
+/* =========================
+   MOBILE MENU
+========================= */
+
+menuButton.addEventListener("click", function () {
+  navLinks.classList.toggle("active");
+});
+
+
+/* =========================
+   MENU NACH KLICK SCHLIESSEN
+========================= */
+
+document.querySelectorAll(".nav-links a").forEach(function (link) {
+
+  link.addEventListener("click", function () {
+    navLinks.classList.remove("active");
+  });
+
+});
